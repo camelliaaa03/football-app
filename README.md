c26f510f849906d763545fc31a387729b4a844d8
Camellia Alfina Hanif <117663224+camelliaaa03@users.noreply.github.com>
2023-11-28 21:05:54 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,70 +1,34 @@
-# Getting Started with Create React App
+# Mini Football App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Aplikasi sederhana ini untuk mencatat data klub dan skor pertandingan sepakbola
 
-## Available Scripts
+## Fitur
+- Input data klub.
+    ![Tampilan Aplikasi](football_1.jpg)
+- Input skor pertandingan
+- Tampilan klasemen
+    ![Tampilan Klasemen](football_2.jpg)
 
-In the project directory, you can run:
+## Cara menjalankan
+1. Clone Repositori ini
+```bash
+git clone https://github.com/camelliaaa03/football-mini-application.git
+cd football-mini-application
+```
+2. Install Dependensi
+npm install
 
-### `npm start`
+3. Jalankan Aplikasi
+npm start
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Penggunaan
+1. Menu input data klub :
+    - Isi Nama Klub dan Kota Klub
+    - Klik button "Save" untuk menyimpan data klub
+2. Menu input skor :
+    - Isi klub dan skor untuk setiap pertandingan
+    - Klik button "Add" untuk menambahkan input skor baru
+    - Klik button "Save" untuk menyimpan pertandingan
+3. View Klasemen :
+    - Lihat klasemen klub dengan jumlah pertandingan, kemenangan, seri, kekalahan, gol yang dicetak, dan gol yang diterima
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,241 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [teams, setTeams] = useState([]);
+  const [matches, setMatches] = useState([]);
+  const [clubName, setClubName] = useState("");
+  const [city, setCity] = useState("");
+  const [matchInputs, setMatchInputs] = useState([
+    { homeTeam: "", awayTeam: "", homeScore: 0, awayScore: 0 },
+  ]);
+
+  const addTeam = () => {
+    if (clubName && city) {
+      if (
+        !teams.find((team) => team.clubName === clubName && team.city === city)
+      ) {
+        setTeams([
+          ...teams,
+          {
+            clubName,
+            city,
+            matches: 0,
+            wins: 0,
+            draws: 0,
+            losses: 0,
+            goalsFor: 0,
+            goalsAgainst: 0,
+          },
+        ]);
+        setClubName("");
+        setCity("");
+      } else {
+        alert("Klub sudah ada!");
+      }
+    } else {
+      alert("Nama klub dan kota harus diisi!");
+    }
+  };
+
+  const addMatch = () => {
+    const isValidMatch = matchInputs.every(
+      (match) =>
+        match.homeTeam &&
+        match.awayTeam &&
+        match.homeScore !== null &&
+        match.awayScore !== null
+    );
+
+    if (isValidMatch) {
+      const newMatches = [...matches, ...matchInputs];
+      setMatches(newMatches);
+
+      setMatchInputs([
+        { homeTeam: "", awayTeam: "", homeScore: 0, awayScore: 0 },
+      ]);
+    } else {
+      alert("Semua field pada form harus diisi!");
+    }
+  };
+
+  const handleMatchInputChange = (index, field, value) => {
+    const updatedMatches = [...matchInputs];
+    updatedMatches[index][field] = value;
+    setMatchInputs(updatedMatches);
+  };
+
+  useEffect(() => {
+    const updatedTeams = [...teams];
+
+    matches.forEach((match) => {
+      const homeTeamIndex = updatedTeams.findIndex(
+        (team) => team.clubName === match.homeTeam
+      );
+      const awayTeamIndex = updatedTeams.findIndex(
+        (team) => team.clubName === match.awayTeam
+      );
+
+      updatedTeams[homeTeamIndex].matches += 1;
+      updatedTeams[awayTeamIndex].matches += 1;
+
+      if (match.homeScore > match.awayScore) {
+        updatedTeams[homeTeamIndex].wins += 1;
+      } else if (match.homeScore === match.awayScore) {
+        updatedTeams[homeTeamIndex].draws += 1;
+        updatedTeams[awayTeamIndex].draws += 1;
+      } else {
+        updatedTeams[awayTeamIndex].wins += 1;
+      }
+
+      updatedTeams[homeTeamIndex].goalsFor += match.homeScore;
+      updatedTeams[homeTeamIndex].goalsAgainst += match.awayScore;
+      updatedTeams[awayTeamIndex].goalsFor += match.awayScore;
+      updatedTeams[awayTeamIndex].goalsAgainst += match.homeScore;
+    });
+
+    setTeams(updatedTeams);
+  }, [matches]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Mini Football App</h1>
+
+      <div>
+        <h2>Menu Input Data Klub</h2>
+        <form>
+          <label>
+            Nama Klub :
+            <input
+              type="text"
+              value={clubName}
+              onChange={(e) => setClubName(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Kota Klub :
+            <input
+              type="text"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </label>
+          <br />
+          <button type="button" onClick={addTeam}>
+            Save
+          </button>
+        </form>
+      </div>
+
+      <div>
+        <h2>Menu Input Skor</h2>
+        {matchInputs.map((match, index) => (
+          <form key={index}>
+            <label>
+              Klub {index * 2 + 1} :
+              <input
+                type="text"
+                value={match.homeTeam}
+                onChange={(e) =>
+                  handleMatchInputChange(index, "homeTeam", e.target.value)
+                }
+              />
+            </label>
+            <br />
+            <label>
+              Klub {index * 2 + 2} :
+              <input
+                type="text"
+                value={match.awayTeam}
+                onChange={(e) =>
+                  handleMatchInputChange(index, "awayTeam", e.target.value)
+                }
+              />
+            </label>
+            <br />
+            <label>
+              Score {index * 2 + 1} :
+              <input
+                type="number"
+                value={match.homeScore}
+                onChange={(e) =>
+                  handleMatchInputChange(
+                    index,
+                    "homeScore",
+                    Number(e.target.value)
+                  )
+                }
+              />
+            </label>
+            <br />
+            <label>
+              Score {index * 2 + 2} :
+              <input
+                type="number"
+                value={match.awayScore}
+                onChange={(e) =>
+                  handleMatchInputChange(
+                    index,
+                    "awayScore",
+                    Number(e.target.value)
+                  )
+                }
+              />
+            </label>
+            <br />
+          </form>
+        ))}
+        <button
+          type="button"
+          onClick={() =>
+            setMatchInputs([
+              ...matchInputs,
+              { homeTeam: "", awayTeam: "", homeScore: 0, awayScore: 0 },
+            ])
+          }
+          style={{ marginRight: "8px" }}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+        <button type="button" onClick={addMatch} style={{ marginLeft: "8px" }}>
+          Save
+        </button>
+      </div>
+
+      <div>
+        <h2>View Klasemen</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Nama Klub</th>
+              <th>Kota</th>
+              <th>Ma</th>
+              <th>Me</th>
+              <th>S</th>
+              <th>K</th>
+              <th>GM</th>
+              <th>GK</th>
+            </tr>
+          </thead>
+          <tbody>
+            {teams.map((team, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{team.clubName}</td>
+                <td>{team.city}</td>
+                <td>{team.matches}</td>
+                <td>{team.wins}</td>
+                <td>{team.draws}</td>
+                <td>{team.losses}</td>
+                <td>{team.goalsFor}</td>
+                <td>{team.goalsAgainst}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
